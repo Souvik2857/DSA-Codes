5ecb84e7829d5c181313edb721be87c6f0f06c24
Souvik Sarkar <sarkarsouvik2343@gmail.com>
2026-03-26 22:29:30 +0530

    Code

--- binary_search_tree.c
+++ binary_search_tree.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 struct node
 {
     int data;
@@ -7,75 +7,117 @@ struct node
     struct node *right;
 };
 // create functions to re-use the code more efficiently
-struct node * createNode(int data){
-    struct node *n;  //creating a node pointer
-    n=(struct node *)malloc(sizeof(struct node)); //Allocating memory for node
-    n->left=NULL;
-    n->data=data;
-    n->right=NULL;
+struct node *createNode(int data)
+{
+    struct node *n;                                 // creating a node pointer
+    n = (struct node *)malloc(sizeof(struct node)); // Allocating memory for node
+    n->left = NULL;
+    n->data = data;
+    n->right = NULL;
     return n;
-    
 }
-void inOrder(struct node *root){
-    if(root!=NULL){
+void inOrder(struct node *root)
+{
+    if (root != NULL)
+    {
         inOrder(root->left);
-        printf("%d ",root->data);
+        printf("%d ", root->data);
         inOrder(root->right);
     }
 }
-void preOrder(struct node *root){
-    if(root!=NULL){
-        printf("%d ",root->data);
+void preOrder(struct node *root)
+{
+    if (root != NULL)
+    {
+        printf("%d ", root->data);
         preOrder(root->left);
         preOrder(root->right);
     }
 }
-void postOrder(struct node *root){
-    if(root!=NULL){
+void postOrder(struct node *root)
+{
+    if (root != NULL)
+    {
         postOrder(root->left);
         postOrder(root->right);
-        printf("%d ",root->data);
+        printf("%d ", root->data);
     }
 }
-int isBST(struct node *root){
-    static struct node *prev=NULL;
-    if(root!=NULL){
-       if(!isBST(root->left)){
-        return 0;
-       }
-       if(prev!=NULL && root->data <= prev->data){
-        return 0;
-       }
-       prev=root;
-       return isBST(root->right);
+struct node *searchBST(struct node *root, int data)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    else if (root->data == data)
+    {
+        return root;
+    }
 
+    else if (data < root->data)
+    {
+        searchBST(root->left, data);
+    }
+    else
+    {
+        searchBST(root->right, data);
+    }
+}
+int isBST(struct node *root)
+{
+    static struct node *prev = NULL;
+    if (root != NULL)
+    {
+        if (!isBST(root->left))
+        {
+            return 0;
+        }
+        if (prev != NULL && root->data <= prev->data)
+        {
+            return 0;
+        }
+        prev = root;
+        return isBST(root->right);
     }
     else
     {
         return 1;
     }
-    
 }
-int main(){
-    struct node *p=createNode(5);
-    struct node *p1=createNode(3);
-    struct node *p2=createNode(6);
-    struct node *p3=createNode(1);
-    struct node *p4=createNode(4);
+int main()
+{
+    struct node *p = createNode(5);
+    struct node *p1 = createNode(3);
+    struct node *p2 = createNode(6);
+    struct node *p3 = createNode(1);
+    struct node *p4 = createNode(4);
     // linking the root node
-    p->left=p1;
-    p->right=p2;
-    p1->left=p3;
-    p1->right=p4;
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
     // in binary search tree Inorder traversal print ascending order of elements
     printf("-----------printing InOrder Traversal-----------\n");
     inOrder(p);
     printf("\n");
-    if(isBST(p)){
+    if (isBST(p))
+    {
         printf("Is a Binary search tree");
     }
-    else{
+    else
+    {
         printf("Is not a binary search tree");
     }
+    printf("\n");
+    struct node *n = searchBST(p, 6);
+    if (n != NULL)
+    {
+        printf("Found: %d", n->data);
+    }
+    else
+    {
+        printf("Element not found");
+    }
+
     return 0;
 }
